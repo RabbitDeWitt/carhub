07b29f2d9c553ff25a6e18cd3c3e5f4fdc7a1aec
David Coelho <96807782+RabbitDeWitt@users.noreply.github.com>
2024-03-14 20:46:41 -0300

    footer added

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -28,9 +28,9 @@ const Hero = () => {
             fill
             className="object-contain"
           />
-          <div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
-
         </div>
+        <div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
+
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,8 @@ const config: Config = {
         "primary-blue": {
           DEFAULT: "#2B59FF",
           100: "#F5F8FF",
-        }
+        },
+        "black-100": "#2B2C35",
       },
     }
   },
